2cc598ea94c4fb52f67ddf2fe77db460b593f06b
darongit <darolbk@gmail.com>
2023-03-27 23:41:30 +0200

    Create CRUD functions, random get question function

--- src/main/java/com/example/questionrandomizer/question/QuestionController.java
+++ src/main/java/com/example/questionrandomizer/question/QuestionController.java
@@ -1,9 +1,8 @@
 package com.example.questionrandomizer.question;
 
+import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,5 +13,54 @@ public class QuestionController {
     @Autowired
     public QuestionController(QuestionService questionService) {
         this.questionService = questionService;
+    }
+
+    @GetMapping("/")
+    public String mainPage() {
+        return questionService.mainPage();
+    }
+    @GetMapping("/question/all")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("/question/{questionId}")
+    public Question getQuestionById(@PathVariable Long questionId) {
+        return questionService.getQuestionById(questionId);
+    }
+
+    @PostMapping("/question/add")
+    public Question addQuestion(@RequestBody Question question) {
+        return questionService.addQuestion(question);
+    }
+
+    @DeleteMapping("/question/delete/{questionId}")
+    public Question deleteQuestion(@PathVariable Long questionId) {
+        return questionService.deleteQuestion(questionId);
+    }
+
+    @PutMapping("/question/put/{questionId}")
+    public Question putQuestion(@RequestBody Question question, @PathVariable Long questionId) {
+        return questionService.putQuestion(question, questionId);
+    }
+
+    @GetMapping("/question/random")
+    public Question getRandomQuestion() {
+        return questionService.getRandomQuestion();
+    }
+
+    @GetMapping("/question/random/{category}")
+    public Question getRandomQuestion(@PathVariable String category) {
+        return questionService.getRandomQuestionByCategory(category.toLowerCase());
+    }
+
+    @GetMapping("/question/reset")
+    public void resetQuestions() {
+        questionService.resetQuestions();
+    }
+
+    @GetMapping("/question/reset/{category}")
+    public void resetQuestionsByCategory(@PathVariable String category) {
+        questionService.resetQuestionsByCategory(category.toLowerCase());
     }
 }
--- src/main/java/com/example/questionrandomizer/question/QuestionService.java
+++ src/main/java/com/example/questionrandomizer/question/QuestionService.java
@@ -12,4 +12,98 @@ public class QuestionService {
     public QuestionService(QuestionRepository questionRepository) {
         this.questionRepository = questionRepository;
     }
+
+    public String mainPage() {
+        StringBuilder result = new StringBuilder();
+        result.append("<html><body>");
+
+        result.append("/api/v1/question/all -> return list of all questions");
+        result.append("<br>");
+        result.append("/api/v1/question/{id} -> show question by id");
+        result.append("<br>");
+        result.append("/api/v1/question/add -> add new question. Use POST with json like {\"category\" : \"categoryName\", \"content\" : \"questions\"}");
+        result.append("<br>");
+        result.append("/api/v1/question/delete/{id} -> delete question by id");
+        result.append("<br>");
+        result.append("/api/v1/question/put/{id} -> update question by id. Use PUT with json like {\"category\" : \"categoryName\", \"content\" : \"questions\"}");
+        result.append("<br>");
+        result.append("/api/v1/question/random -> return random question");
+        result.append("<br>");
+        result.append("/api/v1/question/random/{category} -> return random question by category");
+        result.append("<br>");
+        result.append("/api/v1/question/reset -> make every question unseen");
+        result.append("<br>");
+        result.append("/api/v1/question/reset/{category} -> make every question unseen");
+
+        result.append("</body></html>");
+        return result.toString();
+    }
+
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestionById(Long questionId) {
+        return questionRepository.findById(questionId).orElseThrow();
+    }
+
+    public Question addQuestion(Question question) {
+        return questionRepository.save(question);
+    }
+
+    public Question deleteQuestion(Long questionId) {
+        Question tmp = questionRepository.findById(questionId).orElseThrow();
+        questionRepository.deleteById(questionId);
+        return tmp;
+    }
+
+    public Question putQuestion(Question question, Long questionId) {
+        question.setId(questionId);
+        return questionRepository.save(question);
+    }
+
+    public Question getRandomQuestion() {
+        return getRandomQuestionByCategory("");
+    }
+
+    public Question getRandomQuestionByCategory(String category) {
+        List<Question> toDraw = null;
+
+        if (category.equals("")) {
+            toDraw = questionRepository.findAll().stream()
+                    .filter(question -> !question.isAlreadySeen())
+                    .toList();
+            if (toDraw.isEmpty()) { throw new IllegalStateException("No available questions"); }
+        } else {
+            toDraw = questionRepository.findAll().stream()
+                    .filter(question -> question.getCategory().toLowerCase().equals(category))
+                    .filter(question -> !question.isAlreadySeen())
+                    .toList();
+            if (toDraw.isEmpty()) { throw new IllegalArgumentException("No available questions or wrong category"); }
+        }
+        Question result = toDraw.get((int)(Math.random()*toDraw.size()));
+        result.setAlreadySeen(true);
+        questionRepository.save(result);
+        return result;
+    }
+
+    public void resetQuestions() {
+        resetQuestionsByCategory("");
+    }
+
+    public void resetQuestionsByCategory(String category) {
+        if (category.equals("")) {
+            for (Question question: questionRepository.findAll()) {
+                question.setAlreadySeen(false);
+                questionRepository.save(question);
+            }
+        } else {
+            for (Question question: questionRepository.findAll().stream()
+                    .filter(question -> question.getCategory().toLowerCase().equals(category))
+                    .toList()) {
+                question.setAlreadySeen(false);
+                questionRepository.save(question);
+            }
+        }
+    }
 }
